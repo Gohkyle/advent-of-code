902408115b6e583dd59cc98c8fda9519f216e95d
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-03 21:31:17 +0000

    refactored to cater for right to left search

--- day-1/calibration-document.js
+++ day-1/calibration-document.js
@@ -7,7 +7,8 @@ const joinFirstAndLast = (numArr) => {
 const sumOfCalibrationValues = (calibrationDoc) => {
   const lines = calibrationDoc.split("\n");
   const extractedNumbers = lines.map((line) => {
-    return line.match(/\d/g);
+    const decodedLine = decodeWordNumbers(line);
+    return decodedLine.match(/\d/g);
   });
 
   let sum = 0;
@@ -22,25 +23,39 @@ const sumOfCalibrationValues = (calibrationDoc) => {
 const decodeWordNumbers = (string) => {
   function replacer(match) {
     const numberKey = {
-      one: 1,
-      two: 2,
-      three: 3,
-      four: 4,
-      five: 5,
-      six: 6,
-      seven: 7,
-      eight: 8,
-      nine: 9,
+      one: "1",
+      eno: "1",
+      two: "2",
+      owt: "2",
+      three: "3",
+      eerht: "3",
+      four: "4",
+      ruof: "4",
+      five: "5",
+      evif: "5",
+      six: "6",
+      xis: "6",
+      seven: "7",
+      neves: "7",
+      eight: "8",
+      thgie: "8",
+      nine: "9",
+      enin: "9",
     };
     return numberKey[match];
   }
 
   const regex =
-    /(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/g;
+    /(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/;
 
   const newString = string.replace(regex, replacer);
 
-  return newString;
+  const regexRev = /eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/;
+  const stringRev = newString.split("").reverse().join("");
+
+  const newStringRev = stringRev.replace(regexRev, replacer);
+
+  return newStringRev.split("").reverse().join("");
 };
 
 module.exports = {
@@ -49,6 +64,27 @@ module.exports = {
   decodeWordNumbers,
 };
 
-fs.readFile("./puzzleInput.txt", "utf-8").then((response) => {
-  console.log(sumOfCalibrationValues(response));
-});
+function answer() {
+  fs.readFile("./puzzleInput.txt", "utf-8")
+    .then((response) => {
+      return sumOfCalibrationValues(response);
+    })
+    .then((answer) => {
+      console.log(answer);
+    })
+    .catch((err) => {
+      return err;
+    });
+}
+
+answer();
+
+console.log(
+  sumOfCalibrationValues(`two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen`)
+);
