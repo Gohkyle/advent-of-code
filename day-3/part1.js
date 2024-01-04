3dcf5425a35a9c23f50ee5b4e4ad09b9c2d90218
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-04 20:52:30 +0000

    defined algorithm

---
+++ day-3/part1.js
@@ -0,0 +1,13 @@
+
+const 
+
+//scan all numbers, create total
+
+//scan row, for a number,
+//check length of number, and position
+//create searchbox item which is length of number plus 2, and position is one less than the current
+//check current row search or symbols
+//check previous row with the search box for symbols
+//check next row at with the search box for symbols
+
+//symbol detected move on if not detected store number
