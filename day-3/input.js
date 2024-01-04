2bcea300b97664d9e82bf4b70ae8834597c1359f
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-04 21:13:57 +0000

    created new data folder

--- day-3/input.js
+++ day-3/input.js
@@ -1,13 +1,10 @@
 const fs = require("node:fs/promises");
 
-fs.readFile(`${__dirname}/test-input.txt`, "utf-8")
+fs.readFile(`${__dirname}/data/test-input.txt`, "utf-8")
   .then((response) => response.split("\n"))
   .then((response) => {
-    return response.map((row) => row.split(""));
-  })
-  .then((response) => {
     fs.writeFile(
-      `${__dirname}/test-input.json`,
+      `${__dirname}/data/test-input.json`,
       JSON.stringify(response),
       "utf-8"
     );
