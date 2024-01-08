a0301e3300e259b12ef8a53e9348d6ea88be308d
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-08 22:42:51 +0000

    function input to retrieve answer to submit to advent of code

--- day-3/gearRatios.js
+++ day-3/gearRatios.js
@@ -1,8 +1,17 @@
 const { getTotal, getNonPartsTotal } = require("./part1");
 
 const input = require("./data/input.json");
+const { findGearParts, findGears, sumGearRatios } = require("./part2");
 function answerPart1(data) {
   return getTotal(data) - getNonPartsTotal(data);
 }
 
 console.log(answerPart1(input));
+
+function answerPart2(data) {
+  const parts = findGearParts(data);
+  const gears = findGears(parts);
+
+  return sumGearRatios(gears);
+}
+console.log(answerPart2(input));
