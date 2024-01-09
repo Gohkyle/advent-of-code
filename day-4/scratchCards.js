547e9aa110f1ba8748b601f6641d139565bfdf2a
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-09 18:45:40 +0000

    puzzleinput assertion, correctly submitted to advent of code

--- day-4/scratchCards.js
+++ day-4/scratchCards.js
@@ -1,9 +1,11 @@
 const { convertTxtToJSONFile } = require("./data/convertTxtToJson");
 const { part1Answer } = require("./part1");
+const { part2Answer } = require("./part2");
 
 const dataSet = "puzzle-input";
 
 convertTxtToJSONFile(dataSet).then(() => {
   const scratchCards = require(`./data/${dataSet}.json`);
-  console.log(part1Answer(scratchCards));
+  console.log("part1:", part1Answer(scratchCards));
+  console.log("part2:", part2Answer(scratchCards));
 });
