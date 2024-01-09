9801020e6a674a2ddb907609f28e1a762fa46caa
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-09 16:02:12 +0000

    file set up

---
+++ day-4/part1.js
@@ -0,0 +1,8 @@
+const findMatches = () => {
+  // filter through winning numbers, use array.includes to
+};
+
+const calcPoints = () => {
+  //from a list of matching numbers count length
+  // return sum of 2 to the power of length
+};
