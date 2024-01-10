7c418d108af585ebdefee30a6ab8c9c74ad54196
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-10 18:12:10 +0000

    submitted answer to advent of code correctly

--- day-5/ifYouGiveASeedAFertilizer.js
+++ day-5/ifYouGiveASeedAFertilizer.js
@@ -1,6 +1,7 @@
 const { txtToJSON } = require("./data/txtToJSON");
 const { part1Answer } = require("./part1");
 
-txtToJSON(`${__dirname}/data/puzzle-input`).then((data) => {
+txtToJSON(`${__dirname}/data/puzzle-input`).then(() => {
+  const data = require("./data/puzzle-input.json");
   console.log(part1Answer(data));
 });
