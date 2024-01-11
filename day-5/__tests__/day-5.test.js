65262286ae619a0fd384acfd85dfaf578f711daa
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-11 16:33:20 +0000

    curried convertF, extracted functionality to reuse elsewhere

--- day-5/__tests__/day-5.test.js
+++ day-5/__tests__/day-5.test.js
@@ -10,10 +10,23 @@ const {
 const mock = require("mock-fs");
 
 const fs = require("fs");
-const { seedToSoil, convert, findMin, getLocation } = require("../part1");
+const {
+  seedToSoil,
+  convert,
+  findMin,
+  getLocation,
+  mapNumber,
+  convertF,
+  soilToFert,
+  fertToWater,
+  waterToLight,
+  lightToTemp,
+  tempToHumidity,
+  humidityToLocation,
+} = require("../part1");
 
 const testData = require("../data/test-input.json");
-const { setNewSeed, updateSeedOnAlmanac, part2Answer } = require("../part2");
+const { setNewSeed, convertRanges } = require("../part2");
 describe("txtToJSON.js", () => {
   describe("getMapRegex()", () => {
     test("takes a string, returns regex expression with embedded string ", () => {
@@ -133,7 +146,7 @@ describe("txtToJSON.js", () => {
   });
 });
 
-describe("part1.js", () => {
+describe.only("part1.js", () => {
   describe("seedToSoil()", () => {
     test("convert seed to soil from a one line map, adds to soil key", () => {
       const sampleData = {
@@ -242,51 +255,59 @@ describe("part1.js", () => {
         sampleDataWithFert
       );
     });
-    test("this was failing, when I had else statements", () => {
-      const sampleDataWithWater = {
-        "fertilizer-to-water": [
-          ["49", "53", "8"],
-          ["0", "11", "42"],
-          ["42", "0", "7"],
-          ["57", "7", "4"],
-        ],
-        fertilizer: [81, 53, 57, 52],
-        water: [81, 49, 53, 41],
+  });
+  describe("convertF", () => {
+    test("returns a function", () => {
+      expect(typeof convertF("seed", "soil")).toBe("function");
+    });
+    test("function returned, can do the same as seedToSoil", () => {
+      const sampleData = {
+        seed: ["14", "13"],
+        "seed-to-soil": [["50", "13", "2"]],
       };
-      const sampleWithFert = {
-        "fertilizer-to-water": [
-          ["49", "53", "8"],
-          ["0", "11", "42"],
-          ["42", "0", "7"],
-          ["57", "7", "4"],
-        ],
-        fertilizer: [81, 53, 57, 52],
+      const sampleDataWithSoil = {
+        seed: ["14", "13"],
+        "seed-to-soil": [["50", "13", "2"]],
+        soil: [51, 50],
       };
+      const facSeedToSoil = convertF("seed", "soil");
+
+      expect(facSeedToSoil(sampleData)).toEqual(sampleDataWithSoil);
+    });
+    test("function returned, can map soil to fertilizer", () => {
+      const sampleData = {
+        seed: ["14", "13"],
+        "seed-to-soil": [["50", "13", "2"]],
+      };
+      const sampleDataWithSoil = {
+        seed: ["14", "13"],
+        "seed-to-soil": [["50", "13", "2"]],
+        soil: [51, 50],
+      };
+      const facSeedToSoil = convertF("seed", "soil");
+
+      expect(facSeedToSoil(sampleData)).toEqual(sampleDataWithSoil);
+    });
+  });
+  describe("mapNumber()", () => {
+    test("takes a number and a bunch of ranges and returns new number", () => {
+      const ranges = [["1", "8", "2"]];
+
+      expect(mapNumber(9, ranges)).toBe(2);
+    });
+    test("takes a number and a bunch of ranges and returns the number if no map", () => {
+      const ranges = [["90, 12, 9"]];
+
+      expect(mapNumber(9, ranges)).toBe(9);
     });
   });
   describe("data assertion", () => {
-    const testDataWithSoil = convert("seed", "soil", testData);
-    const testDataWithFertilizer = convert(
-      "soil",
-      "fertilizer",
-      testDataWithSoil
-    );
-    const testDataWithWater = convert(
-      "fertilizer",
-      "water",
-      testDataWithFertilizer
-    );
-    const testDataWithLight = convert("water", "light", testDataWithWater);
-    const testDataWithTemperature = convert(
-      "light",
-      "temperature",
-      testDataWithLight
-    );
-    const testDataWithHumidity = convert(
-      "temperature",
-      "humidity",
-      testDataWithTemperature
-    );
+    const testDataWithSoil = seedToSoil(testData);
+    const testDataWithFertilizer = soilToFert(testDataWithSoil);
+    const testDataWithWater = fertToWater(testDataWithFertilizer);
+    const testDataWithLight = waterToLight(testDataWithWater);
+    const testDataWithTemperature = lightToTemp(testDataWithLight);
+    const testDataWithHumidity = tempToHumidity(testDataWithTemperature);
 
     test("fertilizer-to-water", () => {
       const sampleDataWithWater = {
@@ -309,43 +330,37 @@ describe("part1.js", () => {
         fertilizer: [81, 53, 57, 52],
       };
 
-      expect(convert("fertilizer", "water", sampleWithFert)).toEqual(
-        sampleDataWithWater
-      );
+      expect(fertToWater(sampleWithFert)).toEqual(sampleDataWithWater);
     });
     test("water-to-light", () => {
       const updatedWithLight = {
         ...testDataWithWater,
         light: [74, 42, 46, 34],
       };
-      expect(convert("water", "light", testDataWithWater)).toEqual(
-        updatedWithLight
-      );
+      expect(waterToLight(testDataWithWater)).toEqual(updatedWithLight);
     });
     test("light-to-temperature", () => {
       const updatedWithTemperature = {
         ...testDataWithLight,
         temperature: [78, 42, 82, 34],
       };
-      expect(convert("light", "temperature", testDataWithLight)).toEqual(
-        updatedWithTemperature
-      );
+      expect(lightToTemp(testDataWithLight)).toEqual(updatedWithTemperature);
     });
     test("temperature-to-humidity", () => {
       const updatedWithHumidity = {
         ...testDataWithTemperature,
         humidity: [78, 43, 82, 35],
       };
-      expect(
-        convert("temperature", "humidity", testDataWithTemperature)
-      ).toEqual(updatedWithHumidity);
+      expect(tempToHumidity(testDataWithTemperature)).toEqual(
+        updatedWithHumidity
+      );
     });
     test("humidity-to-location", () => {
       const updatedWithLocation = {
         ...testDataWithHumidity,
         location: [82, 43, 86, 35],
       };
-      expect(convert("humidity", "location", testDataWithHumidity)).toEqual(
+      expect(humidityToLocation(testDataWithHumidity)).toEqual(
         updatedWithLocation
       );
     });
@@ -356,6 +371,7 @@ describe("part1.js", () => {
       expect(findMin(numbers)).toBe(0);
 
       const location = getLocation(testData);
+      console.log(location);
       expect(findMin(location)).toBe(35);
     });
   });
@@ -363,42 +379,61 @@ describe("part1.js", () => {
 
 describe("part2.js", () => {
   describe("setNewSeed()", () => {
-    test("returns array of two arrays, first array being the start values, and the second array being the ranges", () => {
+    test("returns new data Object with seedRanges", () => {
       const data = { seed: ["79", "14", "55", "13"] };
-      const newSeeds = [
-        ["79", "55"],
-        ["14", "13"],
-      ];
-      expect(setNewSeed(data)).toEqual(newSeeds);
-    });
-  });
-  describe("updateSeedOnAlmanac()", () => {
-    test("populates the seed with a range of numbers", () => {
-      const data = { seed: ["79", "14", "55", "13"] };
-      const updatedAlmanac = {
-        seed: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+      const updatedData = {
+        seed: [
+          [79, 92],
+          [55, 67],
+        ],
       };
-      expect(updateSeedOnAlmanac(data, 3, 12)).toEqual(updatedAlmanac);
+      expect(setNewSeed(data)).toEqual(updatedData);
+      expect(setNewSeed(data).seed).toHaveLength(data.seed.length / 2);
     });
-    test("returns a new obj", () => {
+    test("returns a new object", () => {
       const data = { seed: ["79", "14", "55", "13"] };
-      expect(updateSeedOnAlmanac(data, 3, 12)).not.toEqual(data);
+
+      expect(setNewSeed(data)).not.toEqual(data);
     });
     test("does not mutate original obj", () => {
       const data = { seed: ["79", "14", "55", "13"] };
       const copyData = { seed: ["79", "14", "55", "13"] };
-      updateSeedOnAlmanac(data, 3, 12);
+
+      setNewSeed(data);
       expect(data).toEqual(copyData);
     });
-    test("works for boths string and number input", () => {
-      const data = { seed: ["79", "14", "55", "13"] };
-      const updatedAlmanac = {
-        seed: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-      };
-      expect(updateSeedOnAlmanac(data, "3", "12")).toEqual(updatedAlmanac);
-    });
   });
-  describe("data test assertion ", () => {
-    expect(part2Answer(testData)).toBe(46);
+  describe("convertRanges()", () => {
+    describe("checks range against map, and produces new ranges", () => {
+      test("mapping converts the entire range", () => {
+        const testData = {
+          seed: [[79, 92]],
+          "seed-to-soil": [["50 79 14"]],
+        };
+        const testResult = {
+          seed: [[79, 92]],
+          "seed-to-soil": [["50 79 14"]],
+          soil: [[[50, 63]]],
+        };
+
+        expect(convertRanges("seed", "soil", testData)).toEqual(testResult);
+      });
+      test("mapping occurs for a part of the range", () => {
+        const testData = {
+          seed: [[79, 92]],
+          "seed-to-soil": [["50 90 9"]],
+        };
+        const testResult = {
+          seed: [[79, 92]],
+          "seed-to-soil": [["50 90 9"]],
+          soil: [
+            [
+              [79, 89],
+              [50, 52],
+            ],
+          ],
+        };
+      });
+    });
   });
 });
--- day-5/part1.js
+++ day-5/part1.js
@@ -1,24 +1,30 @@
-const seedToSoil = (data) => {
-  return convert("seed", "soil", data);
+const mapNumber = (number, maps) => {
+  let newNumber = +number;
+  maps.forEach((map) => {
+    const lowerLimit = +map[1];
+    const upperLimit = lowerLimit + +map[2];
+    const conversion = +map[0] - +map[1];
+
+    if (number < upperLimit && number >= lowerLimit) {
+      newNumber += +conversion;
+    }
+  });
+  return newNumber;
+};
+
+const convertF = (from, to) => {
+  return function fromTo(data) {
+    return convert(from, to, data);
+  };
 };
 
 const convert = (from, to, data) => {
   const values = data[from].map((value) => {
-    let newNumber = +value;
-
-    data[`${from}-to-${to}`].forEach((line) => {
-      const lowerLimit = +line[1];
-      const upperLimit = lowerLimit + +line[2];
-      const conversion = +line[0] - +line[1];
-
-      if (value < upperLimit && value >= lowerLimit) {
-        newNumber += +conversion;
-      }
-    });
-    return newNumber;
+    return mapNumber(value, data[`${from}-to-${to}`]);
   });
   return { ...data, [to]: values };
 };
+
 const findMin = (numArr) => {
   let min = numArr[0];
   for (const number of numArr) {
@@ -29,18 +35,41 @@ const findMin = (numArr) => {
   return min;
 };
 
+const seedToSoil = convertF("seed", "soil");
+const soilToFert = convertF("soil", "fertilizer");
+const fertToWater = convertF("fertilizer", "water");
+const waterToLight = convertF("water", "light");
+const lightToTemp = convertF("light", "temperature");
+const tempToHumidity = convertF("temperature", "humidity");
+const humidityToLocation = convertF("humidity", "location");
+
 const getLocation = (data) => {
-  const soil = convert("seed", "soil", data);
-  const fertilizer = convert("soil", "fertilizer", soil);
-  const water = convert("fertilizer", "water", fertilizer);
-  const light = convert("water", "light", water);
-  const temperature = convert("light", "temperature", light);
-  const humidity = convert("temperature", "humidity", temperature);
-  const location = convert("humidity", "location", humidity);
+  const soil = seedToSoil(data);
+  const fertilizer = soilToFert(soil);
+  const water = fertToWater(fertilizer);
+  const light = waterToLight(water);
+  const temperature = lightToTemp(light);
+  const humidity = tempToHumidity(temperature);
+  const location = humidityToLocation(humidity);
   return location.location;
 };
 
 const part1Answer = (data) => {
   return findMin(getLocation(data));
 };
-module.exports = { seedToSoil, convert, getLocation, findMin, part1Answer };
+module.exports = {
+  seedToSoil,
+  convert,
+  getLocation,
+  findMin,
+  part1Answer,
+  mapNumber,
+  convertF,
+  seedToSoil,
+  soilToFert,
+  fertToWater,
+  waterToLight,
+  lightToTemp,
+  tempToHumidity,
+  humidityToLocation,
+};
