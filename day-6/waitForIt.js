b2b63b787ef860380e1a9d97e48dc5eede6d807f
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-25 19:22:16 +0000

    answer submitted to adventofcode correctly

--- day-6/waitForIt.js
+++ day-6/waitForIt.js
@@ -1,3 +1,8 @@
 const { getTxtToJSON } = require("./data/txtToJSON");
+const fs = require("fs");
+const { part1Answer } = require("./part1");
 
-getTxtToJSON(`${__dirname}/data/test-input`);
+getTxtToJSON(`${__dirname}/data/puzzle-input`).then(() => {
+  const data = require("./data/puzzle-input.json");
+  console.log(part1Answer(data));
+});
