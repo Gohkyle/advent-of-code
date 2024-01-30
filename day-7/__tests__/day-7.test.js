d1a0da0c934f482f564ebdd66179fba0fcf8a1d3
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-30 14:43:46 +0000

    set up day-7, first test

---
+++ day-7/__tests__/day-7.test.js
@@ -0,0 +1,8 @@
+describe("txtToJSON", () => {
+  describe("txtToArr()", () => {
+    test("returns the txt as an array of objects", () => {
+      const txt = `AK213 95
+            QQQ23 901`;
+    });
+  });
+});
