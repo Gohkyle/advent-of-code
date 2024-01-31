a5f13c6c99b78cef08edb29c96f1c3f57cfb054a
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-31 21:22:53 +0000

    printed answer, to submit to advent of code, answer correct

--- day-7/camelCards.js
+++ day-7/camelCards.js
@@ -1,8 +1,10 @@
 const { txtToJSON } = require("../utils/utils");
 const { txtToArr } = require("./data/txtToJSON");
 const { partOneAnswer } = require("./part1");
+const { partTwoAnswer } = require("./part2");
 
 txtToJSON(`${__dirname}/data/puzzle-input`, txtToArr).then(() => {
   const data = require("./data/puzzle-input.json");
   console.log("part1: ", partOneAnswer(data));
+  console.log("part2: ", partTwoAnswer(data));
 });
