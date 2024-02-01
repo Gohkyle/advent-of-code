c58b7c74c6600d3cd24557b9a67ddeafe6866e75
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-02-01 19:30:36 +0000

    part two answer submitted to advent of code, answer correct

--- day-8/hauntedWasteland.js
+++ day-8/hauntedWasteland.js
@@ -1,8 +1,10 @@
 const { txtToJSON } = require("../utils/utils");
 const { formatData } = require("./data/formatData");
 const { countSteps } = require("./part1");
+const { partTwoAnswer } = require("./part2");
 
 txtToJSON(`${__dirname}/data/puzzle-input`, formatData).then(() => {
   const data = require("./data/puzzle-input.json");
   console.log(countSteps(data));
+  console.log(partTwoAnswer(data));
 });
