03adfac5e223c4d0c1b1ed3d59b50addb1f82419
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-02-15 18:45:56 +0000

    attempt to submit to advent of code, call stack maximum is exceeded

---
+++ day-10/pipeMaze.js
@@ -0,0 +1,12 @@
+const { txtToJSON } = require("../utils/utils");
+const { getPipes } = require("./data/formatData");
+const { partOneAnswer, getRoute } = require("./part1");
+
+async function answer() {
+  await txtToJSON(`${__dirname}/data/puzzle-input`, getPipes);
+
+  const data = require("./data/puzzle-input.json");
+  console.log(getRoute(data));
+}
+
+answer();
